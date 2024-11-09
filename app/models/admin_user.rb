b13fcab5a3c90bd79516404645a335e745002fab
obafisoye <dannieoba@gmail.com>
2024-11-08 19:32:56 -0600

    Added customer partial

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,6 +2,7 @@ class AdminUser < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     column_names - %w[encrypted_password reset_password_token reset_password_sent_at remember_created_at image_attachment image_blob]
   end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
